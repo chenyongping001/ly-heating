a583ed03c9647ac0bc11c90e4e61d2f1001a9339
chenyongping <chenyongping001@gmail.com>
2024-03-12 23:18:37 +0800

    allowed Server action Origins

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+    serverActions: {
+      allowedOrigins: ['ly-heating.td.masterpeak.cn', '*.td.masterpeak.cn'],
+    },
+  },
+}
 
 module.exports = nextConfig
+
